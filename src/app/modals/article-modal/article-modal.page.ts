3aac9f00d6430d0734c41d1c1a1b9a2b7823b8bc
KatarinaEgebjerg <katarina.egebjerg.gmail.com>
2023-12-08 11:51:24 +0100

    create modal, modal content

---
+++ src/app/modals/article-modal/article-modal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-modal',
+  templateUrl: './article-modal.page.html',
+  styleUrls: ['./article-modal.page.scss'],
+})
+export class ArticleModalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
